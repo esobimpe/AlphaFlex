b91db30e1fdfc0be4650710262d5f24a332604f7
Eniola Sobimpe <42876585+esobimpe@users.noreply.github.com>
2024-12-22 22:00:43 -0600

    version control

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="alpha_flex",
-    version="1.5.0",
+    version="1.6.0",
     description="A Python library for building growth-focused ETF portfolios.",
     author="Eniola Sobimpe",
     author_email="sobimpeeniola@gmail.com",
