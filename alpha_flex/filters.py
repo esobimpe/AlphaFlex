939927f1f13002c2774e19e51d1519ce3fea3d2f
Eniola Sobimpe <42876585+esobimpe@users.noreply.github.com>
2025-01-12 20:35:46 -0600

    Update filters.py

--- alpha_flex/filters.py
+++ alpha_flex/filters.py
@@ -19,36 +19,5 @@ FILTERS = {
         'InstitutionalOwnership': 'Over 70%',
         'Price': 'Over $5',
         'Float Short': 'Under 5%'
-    },
-    "Buy and Hold": {
-        'Market Cap.': '+Small (over $300mln)',
-        'Current Ratio': 'Over 1.5',
-        'EPS growthnext 5 years': 'Over 10%',
-        'PEG': 'Under 2',
-        'Return on Equity': 'Over +15%',
-        'Beta': 'Under 1.5',
-        '20-Day Simple Moving Average': 'Price above SMA20'
-    },
-    "Bullish": {
-        'EPS growthpast 5 years': 'Positive (>0%)',
-        'EPS growthqtr over qtr': 'Over 20%',
-        'EPS growththis year': 'Over 25%',
-        'EPS growthnext year': 'Over 15%',
-        'EPS growthnext 5 years': 'Positive (>0%)',
-        'Return on Equity': 'Over +15%',
-        'InstitutionalOwnership': 'Over 20%',
-        'Price': 'Over $15',
-        '52-Week High/Low': '90% or more above Low',
-        'RSI (14)': 'Not Oversold (>50)'
-    },
-    "CANSLIM": {
-        'EPS growthpast 5 years': 'Over 20%',
-        'Average Volume': 'Over 500K',
-        'EPS growthqtr over qtr': 'Over 20%',
-        'EPS growththis year': 'Over 20%',
-        'Sales growthpast 5 years': 'Over 20%',
-        'Sales growthqtr over qtr': 'Over 20%',
-        'Net Profit Margin': 'Over 10%',
-        'Current Volume': 'Over 1M'
     }
 }
