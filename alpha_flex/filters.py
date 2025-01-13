8171a1041fcac9598de7ddf2fff28b802f39f000
Eniola Sobimpe <42876585+esobimpe@users.noreply.github.com>
2025-01-12 20:25:15 -0600

    Update filters.py
    TIGHTEN GROWTH FILTERS

--- alpha_flex/filters.py
+++ alpha_flex/filters.py
@@ -2,31 +2,31 @@ FILTERS = {
     "High Growth": {
         'Market Cap.': '+Mid (over $2bln)',
         'EPS growthqtr over qtr': 'Over 25%',
-        'EPS growthnext year': 'Over 25%',
-        'EPS growththis year': 'Over 25%',
+        'EPS growthnext year': 'Over 30%',
+        'EPS growththis year': 'Over 30%',
         'Sales growthqtr over qtr': 'Over 25%',
-        'Average Volume': 'Over 400K',
+        'Average Volume': 'Over 1M',
         'RSI (14)': 'Not Oversold (>50)',
         '200-Day Simple Moving Average': 'Price above SMA200'
     },
     "High Sales Growth": {
         'Debt/Equity': 'Under 0.5',
-        'Gross Margin': 'Over 40%',
-        'Return on Equity': 'Over +15%',
+        'Gross Margin': 'Over 50%',
+        'Return on Equity': 'Over +20%',
         'Sales growthpast 5 years': 'Over 20%',
         'Sales growthqtr over qtr': 'Over 20%',
         'Average Volume': 'Over 200K',
-        'InstitutionalOwnership': 'Over 60%',
+        'InstitutionalOwnership': 'Over 70%',
         'Price': 'Over $5',
         'Float Short': 'Under 5%'
     },
     "Buy and Hold": {
-        'Market Cap.': '+Micro (over $50mln)',
+        'Market Cap.': '+Small (over $300mln)',
         'Current Ratio': 'Over 1.5',
         'EPS growthnext 5 years': 'Over 10%',
-        'PEG': 'Over 1',
+        'PEG': 'Under 2',
         'Return on Equity': 'Over +15%',
-        'Beta': 'Over 1.5',
+        'Beta': 'Under 1.5',
         '20-Day Simple Moving Average': 'Price above SMA20'
     },
     "Bullish": {
@@ -36,17 +36,19 @@ FILTERS = {
         'EPS growthnext year': 'Over 15%',
         'EPS growthnext 5 years': 'Positive (>0%)',
         'Return on Equity': 'Over +15%',
-        'InstitutionalOwnership': 'Over 10%',
+        'InstitutionalOwnership': 'Over 20%',
         'Price': 'Over $15',
         '52-Week High/Low': '90% or more above Low',
         'RSI (14)': 'Not Oversold (>50)'
     },
     "CANSLIM": {
         'EPS growthpast 5 years': 'Over 20%',
+        'Average Volume': 'Over 500K',
         'EPS growthqtr over qtr': 'Over 20%',
         'EPS growththis year': 'Over 20%',
         'Sales growthpast 5 years': 'Over 20%',
         'Sales growthqtr over qtr': 'Over 20%',
+        'Net Profit Margin': 'Over 10%',
         'Current Volume': 'Over 1M'
     }
 }
